bf361404fc27ac1346471eb07a55fe3ee00561c8
Andromeda <stroev.andrew@gmail.com>
2025-12-06 00:55:12 +0000

    Construct vinyl record

---
+++ src/lib/components/Record.svelte
@@ -0,0 +1,5 @@
+<div class="aspect-square rounded-full bg-black flex justify-center items-center">
+	<div class="absolute w-full h-1/100 bg-gray-900"></div>
+	<div class="aspect-square rounded-full bg-gray-700 w-1/4 z-1"></div>
+	<div class="absolute aspect-square rounded-full bg-black w-1/15 z-2"></div>
+</div>
